6afcba21ed45f351883d30b9910dca362aa565f8
Pranjal Srivastava <pranjal11246@gmail.com>
2026-06-08 12:45:07 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int i: nums){
+            freq[i]++;
+        }
+        for(auto pair:freq){
+            if(pair.second>1)return true;
+        }
+        return false;
+    }
+};
