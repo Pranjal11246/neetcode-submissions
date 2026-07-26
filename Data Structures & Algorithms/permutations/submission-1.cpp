ce21ccd2fd7edda215dddb96fc883ccde3a45fea
Pranjal Srivastava <pranjal11246@gmail.com>
2026-07-26 20:22:22 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        if(nums.empty()){
+            return {{}};
+        }
+
+        vector<vector<int>> res;
+
+        vector<int> temp = vector<int>(nums.begin()+1,nums.end());
+        vector<vector<int>> perms = permute(temp);
+
+        for(const auto& p: perms){
+            for(int i=0;i<=p.size();i++){
+                vector<int> p_copy = p;
+                p_copy.insert(p_copy.begin()+i,nums[0]);
+                res.push_back(p_copy);
+            }
+        }
+        return res;
+    }
+};
